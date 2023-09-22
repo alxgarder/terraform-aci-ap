de38062e3f9813095db0ecfe0409089c2d6afd70
alxgarder <30953248+alxgarder@users.noreply.github.com>
2023-09-22 10:31:41 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "app_name" {
+  description = "name of the application folder."
+}
+
+variable "tenant_dn" {
+  description = "dn of the tenant."
+}
+
+variable "description" {
+  description = "Description of the application."
+  default = ""
+}
